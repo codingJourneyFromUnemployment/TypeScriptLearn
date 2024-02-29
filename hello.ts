930a0bb94a2652cbeb9daab78b128800df8f6c3e
laidOffWorkerLearnsToCode <kwg6qlyv@hotmail.com>
2024-02-29 14:01:41 +0800

    init

---
+++ hello.ts
@@ -0,0 +1,30 @@
+// function sayHello(person: string){
+//   return 'Hello, ' + person;
+// }
+
+// const user = [1,2,3]
+// console.log(sayHello(user));
+
+// let isDone: boolean = false;
+// const createdByNewBoolean: boolean = new Boolean(1);
+
+// let myName: string = 'Tom';
+// const myAge: number = 25;
+
+// const sentence: string = `Hello, my name is ${myName}. I'll be ${myAge + 1} years old next month.`;
+
+// console.log(sentence);
+
+// function alertName(): void {
+//   alert('My name is Tom');
+// }
+
+// const num: number | undefined = undefined;
+
+// const num: any = undefined
+// console.log(num.foo());
+// console.log(num.foo.bar());
+
+// const num = null;
+// console.log(num.foo());
+// console.log(num.foo.bar());
