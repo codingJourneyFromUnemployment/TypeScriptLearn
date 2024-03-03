150baf11fb465b112f2529a30d10fbebac86be3a
codingJourneyFromUnemployment <kwg6qlyv@hotmail.com>
2024-03-03 14:34:15 +0800

    type alias

---
+++ BuildinObject.ts
@@ -0,0 +1,17 @@
+const b: Boolean = new Boolean(1);
+const e: Error = new Error('Error occurred');
+const d: Date = new Date();
+const r: RegExp = /[a-z]/;
+
+let body: HTMLElement = document.body;
+let allDiv: NodeList = document.querySelectorAll('div');
+document.addEventListener('click', function(e: MouseEvent) {
+  // Do something
+});
+
+Math.pow(10, 2);
+
+document.addEventListener('click', function(e: MouseEvent) {
+  console.log(e.clientX, e.clientY);
+});
+
