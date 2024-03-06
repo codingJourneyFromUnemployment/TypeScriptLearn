61d783052345f60ce04155459b7c0738f5d645ad
codingJourneyFromUnemployment <kwg6qlyv@hotmail.com>
2024-03-06 22:48:34 +0800

    class and object

--- ClassAndInterface.ts
+++ ClassAndInterface.ts
@@ -2,6 +2,20 @@ interface Alarm {
   alert(): void;
 }
 
+class Door {
 
+}
+
+class SecurityDoor extends Door implements Alarm {
+  alert(){
+    console.log('SecurityDoor alert');
+  }
+}
+
+class Car implements Alarm {
+  alert(){
+    console.log('Car alert');
+  }
+}
 
 
